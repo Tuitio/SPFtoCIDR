d338008b126d7589d128104985def0638ce3a680
Tuitio <macavity@tuitio.co.uk>
2014-01-12 21:14:41 +0000

    Add php script to build CIDR list from SPF records

---
+++ spfcidr.php
@@ -0,0 +1,50 @@
+#!/usr/bin/php
+<?php
+
+$whitelist = array(
+    'google.com',
+    'ebay.co.uk',
+    'amazon.co.uk'
+);
+
+$exclude = array(
+    'salesforce.com'
+);
+
+foreach ($whitelist as $mxdomain) {
+    getIpAddresses($mxdomain, $exclude);
+}
+
+
+/**
+ * Retrieve all records that may contain ip addresses. Prints out domain as a comment
+ * followed by cidr lookup table style printing of ip addresses/blocks.
+ *
+ * @param string $domain  Domain to add to whitelist
+ * @param array  $exclude Array of domains to exclude from whitelist
+ * @param array  $unique  Array of cidrs already retrieved
+ *
+ * @return void
+ */
+function getIpAddresses($domain, Array $exclude = array(), Array &$unique=array())
+{
+    printf("# %s\n", $domain);
+    foreach (dns_get_record($domain, DNS_TXT) as $resultset) {
+        preg_match_all('/ip(4|6):([^ ]+)/', $resultset['txt'], $matches);
+        foreach ($matches[2] as $record) {
+            if (!in_array($record, $unique)) {
+                $unique[] = $record;
+                printf("%s\tpermit\n", $record);
+            }
+        }
+        preg_match_all('/include:([^ ]+)/', $resultset['txt'], $matches);
+        if (is_array($matches[1])) {
+            foreach ($matches[1] as $subdomain) {
+                if (!in_array($subdomain, $exclude)) {
+                    getIpAddresses($subdomain, $exclude, $unique);
+                }
+            }
+        }
+
+    }
+}
